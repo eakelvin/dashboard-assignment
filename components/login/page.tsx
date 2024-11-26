2513f7bb8194790b4c702fefe568816ddf9b2e10
Kelvin Akaba <akabakelvin@gmail.com>
2024-11-26 08:40:26 +0000

    test login

--- components/login/page.tsx
+++ components/login/page.tsx
@@ -6,87 +6,85 @@ import Image from 'next/image'
 
 const Login = () => {
     return (
-        <div>
-            <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
-                <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
-                    <div className="lg:w-1/2 p-6 sm:p-12">
-                        <div>
-                            <img src="https://drive.google.com/uc?export=view&id=1MFiKAExRFF0-2YNpAZzIu1Sh52J8r16v"
-                                className="w-mx-auto" />
+        <div className="h-full w-full flex flex-row">
+            <main className="relative basis-full flex justify-center h-screen items-start sm:items-center min-[1020px]:basis-1/2 px-5 pt-20 pb-5">
+                <div className="w-full max-w-md">
+
+                    <div className='mb-6'>
+                        <h3 className="font-semibold text-3xl mb-3 text-lime-400">
+                            Login
+                        </h3>
+                        <h5 className="text-gray-500 font-light">
+                            How do i get started?
+                        </h5>
+                    </div>
+
+                    <form
+                        className="w-full flex flex-col gap-4"
+                    // onSubmit={handleSubmit(onSubmit)}
+                    >
+                        <div className="">
+                            <label
+                                htmlFor="email"
+                                className="font-medium text-lime-400"
+                            >
+                                Email
+                            </label>
+                            <Input
+                                type="email"
+                                placeholder="Enter your email"
+                                className="rounded-full !ring-0 !ring-offset-0"
+                            />
                         </div>
-                        <div className="mt-12 flex flex-col items-center">
-                            <div className="w-full flex-1 mt-8">
-                                <div className="flex flex-col items-center">
-                                    <button
-                                        className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-green-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
-                                        <div className="bg-white p-2 rounded-full">
-                                            <svg className="w-4" viewBox="0 0 533.5 544.3">
-                                                <path
-                                                    d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z"
-                                                    fill="#4285f4" />
-                                                <path
-                                                    d="M272.1 544.3c73.4 0 135.3-24.1 180.4-65.7l-87.7-68c-24.4 16.6-55.9 26-92.6 26-71 0-131.2-47.9-152.8-112.3H28.9v70.1c46.2 91.9 140.3 149.9 243.2 149.9z"
-                                                    fill="#34a853" />
-                                                <path
-                                                    d="M119.3 324.3c-11.4-33.8-11.4-70.4 0-104.2V150H28.9c-38.6 76.9-38.6 167.5 0 244.4l90.4-70.1z"
-                                                    fill="#fbbc04" />
-                                                <path
-                                                    d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z"
-                                                    fill="#ea4335" />
-                                            </svg>
-                                        </div>
-                                        <span className="ml-4">
-                                            Sign In with Google
-                                        </span>
-                                    </button>
-
-                                </div>
-
-                                <div className="my-12 border-b text-center">
-                                    <div
-                                        className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
-                                        Or sign In with Cartesian E-mail
-                                    </div>
-                                </div>
-
-                                <div className="mx-auto max-w-xs">
-                                    <input
-                                        className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                                        type="email" placeholder="Email" />
-                                    <input
-                                        className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                                        type="password" placeholder="Password" />
-                                    <button
-                                        className="mt-5 tracking-wide font-semibold bg-green-400 text-white-500 w-full py-4 rounded-lg hover:bg-green-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
-                                        <svg className="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
-                                            stroke-linecap="round" stroke-linejoin="round">
-                                            <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
-                                            <circle cx="8.5" cy="7" r="4" />
-                                            <path d="M20 8v6M23 11h-6" />
-                                        </svg>
-                                        <span className="ml-">
-                                            Sign In
-                                        </span>
-                                    </button>
-                                    <p className="mt-6 text-xs text-gray-600 text-center">
-                                        I agree to abide by Cartesian Kinetics
-                                        <a href="#" className="border-b border-gray-500 border-dotted">
-                                            Terms of Service
-                                        </a>
-                                        and its
-                                        <a href="#" className="border-b border-gray-500 border-dotted">
-                                            Privacy Policy
-                                        </a>
-                                    </p>
-                                </div>
+                        <div className="">
+                            <label
+                                htmlFor="password"
+                                className="font-medium text-lime-400"
+                            >
+                                Password
+                            </label>
+                            <div className="relative flex items-center">
+                                <Input
+                                    type='password'
+                                    placeholder="Enter password"
+                                    className='rounded-full'
+                                    required
+                                />
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-[18px] h-[18px] absolute right-4 cursor-pointer" viewBox="0 0 128 128">
+                                    <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
+                                </svg>
                             </div>
                         </div>
-                    </div>
-                    <div className="flex-1 bg-green-100 text-center hidden lg:flex">
-                        <div className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
-                            style={{ backgroundImage: "url('https://drive.google.com/uc?export=view&id=1KZ_Ub_2lZ0dHbKV0fAIhxVhiQA183RCz')" }}>
+                        <div className="flex flex-wrap items-center justify-between">
+                            <div className="flex items-center">
+                                <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 shrink-0 text-blue-600 focus:ring-blue-500 border-gray-300 rounded" />
+                                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-500">
+                                    Remember me
+                                </label>
+                            </div>
+
+                            <Link href="" className="text-sm text-lime-400 hover:underline font-semibold">
+                                Forgot password?
+                            </Link>
                         </div>
-                    </div>
+                        <div className="mt-4">
+                            <Button className='w-full bg-gradient-to-r from-lime-950 to-lime-400 rounded-full'>
+                                Login
+                            </Button>
+                        </div>
+                    </form>
+                </div>
+            </main>
+            <div className="min-[1020px]:flex box-border fixed w-[50vw] right-3 top-3 bottom-3 flex-col items-center justify-between hidden h-[97vh] rounded-2xl p-8 basis-1/2">
+                <div className="flex flex-col items-start">
+                    <Image
+                        src={""}
+                        className="h-16"
+                        alt="logo"
+                        width={100}
+                        height={64}
+                        loading="lazy"
+                    />
                 </div>
             </div>
         </div>
