4e02859a114e27352d0c036f102ddc03034555a7
Kelvin Akaba <akabakelvin@gmail.com>
2024-11-27 22:42:09 +0000

    test api call

--- components/dashboard/dashboard.tsx
+++ components/dashboard/dashboard.tsx
@@ -1,11 +1,30 @@
 "use client"
+import { getReports } from '@/utils/api'
+import { redirect } from 'next/navigation'
 import React, { useState, useEffect } from 'react'
 
-type DashboardProps = {
-    reports: Summary
-}
-
 const Dashboard = () => {
+    const token = localStorage.getItem("token")
+    const [report, setReport] = useState<Summary | null>(null)
+    const [loading, setLoading] = useState(true)
+
+    if (!token) {
+        redirect("/login")
+    }
+
+    useEffect(() => {
+        const refreshReports = async () => {
+            try {
+                const reports = await getReports(token)
+                console.log(reports);
+                setReport(reports)
+            } catch (error: any) {
+                console.log(error);
+            }
+        }
+
+        refreshReports()
+    }, [])
 
     return (
         <div>
